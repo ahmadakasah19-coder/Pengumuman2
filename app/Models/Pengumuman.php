696585e4e04de579ad570e8342ac3ba521ed77c8
ahmadakasah19-coder <ahmadakasah19@gmail.com>
2025-09-24 21:48:30 +0800

    php model

--- app/Models/Pengumuman.php
+++ app/Models/Pengumuman.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Pengumuman extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'judul',
+        'isi',
+        'tanggal_publikasi',
+        'status_publikasi',
+    ];
 }
